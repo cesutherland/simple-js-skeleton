a34dcfcd65e3b91a4b3b50833ae61caf94f8e588
Carl Sutherland <carl@humblesoftware.com>
2012-08-17 12:18:36 -0400

    Added collection test.

---
+++ test/models/collection_test.js
@@ -0,0 +1,11 @@
+var
+  Collection = require('models/Collection');
+
+describe('Collection', function () {
+  beforeEach(function () {
+    this.collection = new Collection();
+  });
+
+  it('instantiates', function () {});
+});
+
